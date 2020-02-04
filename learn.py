95af8a1e1c75fc9b2332fa6ad455c169dd1ceda3
smallelephant9516 <52638056+smallelephant9516@users.noreply.github.com>
2020-02-04 03:28:22 -0500

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,4 +1,8 @@
 import numpy as np
+from datetime import datetime
+start=datetime.now()
+
+
 def get_index(lst,item):
     return [i for i in range(len(lst)) if lst[i] == item]
 def combination(n):
@@ -8,7 +12,7 @@ def combination(n):
         s='0'*(n-len(s))+s
         loop.append(list(s))
     return loop
-def findByRow(mat, row):
-    return np.where((mat == row).all(1))[0]
 a=combination(3)
-print(findByRow(a, ['0','1','1']))
+#Statements
+
+print (datetime.now()-start)
